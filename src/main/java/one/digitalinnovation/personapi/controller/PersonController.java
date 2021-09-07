c5a1205db26f4b3a39794869826adf6b580cb742
pzoliveira <pzoliveira@outlook.com>
2021-09-07 18:12:27 -0300

    Third commit

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,15 +1,36 @@
 package one.digitalinnovation.personapi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.Builder;
+import lombok.Data;
+import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.repository.PersonRepository;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
+	
+	private PersonRepository personRepository;
 
-	@GetMapping
-	public String getBook() {
-		return "API Test!";
+	@Autowired
+	public PersonController(PersonRepository personRepository) {
+		this.personRepository = personRepository;
+	}
+
+	@PostMapping
+	public MessageResponseDTO createPerson (@RequestBody Person person) {
+		Person savedPerson = personRepository.save(person);
+		return MessageResponseDTO
+				.builder()
+				.message("Created person with ID " + savedPerson.getId())
+				.build();
+		
 	}
 }
